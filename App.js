777dbf316875e7fed7a0dcce9c33a4db0a4f4f9b
Tarun Reddy <tarunreddy099@gmail.com>
2019-06-06 20:45:12 +0530

    login added

--- App.js
+++ App.js
@@ -7,40 +7,73 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {View, Text, TouchableOpacity, TextInput, StyleSheet} from 'react-native';
 
-const instructions = Platform.select({
-    ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-    android:
-        'Double tap R on your keyboard to reload,\n' +
-        'Shake or press menu button for dev menu',
-});
 
 type Props = {};
 export default class App extends Component<Props> {
+    state = {
+        email: '',
+        password: ''
+    }
+    handleEmail = (text) => {
+        this.setState({ email: text })
+    }
+    handlePassword = (text) => {
+        this.setState({ password: text })
+    }
+    login = (email, pass) => {
+        alert('email: ' + email + ' password: ' + pass)
+    }
+
     render() {
         return (
-            <View style={styles.container}>
-                <Text style={styles.welcome}> WELCOME TO NECTAR</Text>
-                <Text style={styles.instructions}>->DONATE ->BUY ->SAVE</Text>
+            <View style = {styles.container}>
+                <TextInput style = {styles.input}
+                           underlineColorAndroid = "transparent"
+                           placeholder = "Email"
+                           placeholderTextColor = "#9a73ef"
+                           autoCapitalize = "none"
+                           onChangeText = {this.handleEmail}/>
 
+                <TextInput style = {styles.input}
+                           underlineColorAndroid = "transparent"
+                           placeholder = "Password"
+                           placeholderTextColor = "#9a73ef"
+                           autoCapitalize = "none"
+                           onChangeText = {this.handlePassword}/>
+
+                <TouchableOpacity
+                    style = {styles.submitButton}
+                    onPress = {
+                        () => this.login(this.state.email, this.state.password)
+                    }>
+                    <Text style = {styles.submitButtonText}> Submit </Text>
+                </TouchableOpacity>
             </View>
         );
     }
 }
-
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
+        paddingTop: 23
     },
-    welcome: {
-        fontSize: 50,
-        textAlign: 'center',
-        margin: 10,
+    input: {
+        margin: 15,
+        height: 40,
+        borderColor: '#7a42f4',
+        borderWidth: 1
     },
+    submitButton: {
+        backgroundColor: '#7a42f4',
+        padding: 10,
+        margin: 15,
+        height: 40,
+    },
+    submitButtonText:{
+        color: 'white'
+    },
+
     instructions: {
         textAlign: 'center',
         fontSize: 20,
